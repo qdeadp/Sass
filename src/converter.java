6c68d91dba8d0ff9f8037837b0b91e06fce5475b
qdeadp <aleksandrerm@hotmail.com>
2016-10-21 01:38:50 +0300

    meh

--- src/converter.java
+++ src/converter.java
@@ -11,7 +11,13 @@ public class Convert {
         final double CM_PER_INCH = 2.54;
         final int IN_PER_FOOT = 12;
         Scanner in = new Scanner(System.in);
-// prompt the user and get the value System.out.print("Exactly how many cm? "); cm = in.nextDouble();
-// convert and output the result inches = (int) (cm / CM_PER_INCH); feet = inches / IN_PER_FOOT; remainder = inches % IN_PER_FOOT; System.out.printf("%.2f cm = %d ft, %d in\n", cm, feet, remainder);
+// prompt the user and get the value
+        System.out.print("Exactly how many cm? ");
+        cm = in.nextDouble();
+// convert and output the result
+        inches = (int) (cm / CM_PER_INCH);
+        feet = inches / IN_PER_FOOT;
+        remainder = inches % IN_PER_FOOT;
+        System.out.printf("%.2f cm = %d ft, %d in\n", cm, feet, remainder);
     }
 }
